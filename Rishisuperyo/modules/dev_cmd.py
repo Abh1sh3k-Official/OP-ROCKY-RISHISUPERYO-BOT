44ea2450fc65f66db37f18cb3ce02b63c4d84367
Rishisuperyo OP <73427470+Rishisuperyo-Op@users.noreply.github.com>
2021-05-23 13:29:54 +0530

    Yep bro

---
+++ Rishisuperyo/modules/dev_cmd.py
@@ -0,0 +1,30 @@
+__help__ = """
+Here is all the staff's commands.
+ 🔥 Developers only
+*OWNER*
+⚡ /broadcast: Send a broadcast message to all chat that i'm currently in.
+⚡ /staffids: Get all staff's you have.
+⚡ /ip: Sends the bot's IP address to ssh in if necessary (PM only).
+*DEV USERS*
+⚡ /gitpull: Pull latest update.
+⚡ /reboot: Restart the bot.
+⚡ /dbcleanup: Clean my invalid database.
+⚡ /leavemutedchats: Leave all chats where i can't send message.
+⚡ /leave <chatid>: Tell me to leave the given group. (alias /leavechat /leavegroup).
+⚡ /stats: List of all blacklists, filters, federations, gbans, etc from all group.
+⚡ /getlink <chatid>: Get chat invite link.
+⚡ /sysinfo: Get my system info.
+*SUDO USERS*
+⚡ /snipe <chatid> <string>: Tell me to send a message to the given chat.
+⚡ /echo <string>: Like snipe but on the current chat.
+⚡ /chatlist: Get the list of chat that i'm currently in.
+⚡ /ping: Start a ping test.
+⚡ /speedtest: Start a speedtest from my server.
+*SUPPORT USERS*
+⚡ /gban <userid>: global ban a user.
+⚡ /ungban <userid>: remove currently gbanned user.
+⚡ /gbanlist: Get the list of currently gbanned users.
+"""
+
+
+__mod_name__ = "DEVS"
