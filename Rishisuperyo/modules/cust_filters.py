ceb85e9b93f4f91fefe740a18022ea336b0496a3
Rishisuperyo OP <73427470+Rishisuperyo-Op@users.noreply.github.com>
2021-05-24 18:56:28 +0530

    .

--- Rishisuperyo/modules/cust_filters.py
+++ Rishisuperyo/modules/cust_filters.py
@@ -552,7 +552,7 @@ __help__ = """
 is mentioned. If you reply to a sticker with a keyword, the bot will reply with that sticker. NOTE: all filter \
 keywords are in lowercase. If you want your keyword to be a sentence, use quotes. eg: /filter "hey there" How you \
 doin?
- ➩ /stop <filter keyword>: Stop that filter.
+ ⚡ /stop <filter keyword>: Stop that filter.
 
 *Chat creator only:*
  ⚡ /rmallfilter: Stop all chat filters at once.
