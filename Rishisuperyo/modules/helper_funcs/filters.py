cab67042d11fd17043ace3ae329ca779e93460c0
Rishisuperyo OP <73427470+Rishisuperyo-Op@users.noreply.github.com>
2021-05-24 18:06:58 +0530

    Hmm.mm

--- Rishisuperyo/modules/helper_funcs/filters.py
+++ Rishisuperyo/modules/helper_funcs/filters.py
@@ -5,7 +5,7 @@ from Rishisuperyo import SUPPORT_USERS, SUDO_USERS, DEV_USERS
 
 
 class CustomFilters(object):
-    class __Supporters(BaseFilter):
+    class _Supporters(BaseFilter):
         def filter(self, message: Message):
             return bool(
                 message.from_user
@@ -16,7 +16,7 @@ class CustomFilters(object):
                 and message.from_user.id in DEV_USERS
             )
 
-    support_filter = __Supporters()
+    support_filter = _Supporters()
 
     class _Sudoers(BaseFilter):
         def filter(self, message: Message):
